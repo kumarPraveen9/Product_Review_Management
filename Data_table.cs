42cac4efa31d5c98a9f2625e0386c12fc0f084d6
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-02-05 22:14:55 +0530

    adding a method to fetch all values where islike value is true

--- Data_table.cs
+++ Data_table.cs
@@ -20,39 +20,49 @@ namespace Product_Review_Management
             table.Columns.Add("IsLike");
 
             table.Rows.Add("1","9","5","Excellent","true");
-            table.Rows.Add("2", "9", "5", "Excellent", "true");
-            table.Rows.Add("3", "9", "5", "Excellent", "true");
-            table.Rows.Add("4", "9", "5", "Excellent", "true");
-            table.Rows.Add("5", "9", "5", "Excellent", "true");
-            table.Rows.Add("6", "9", "5", "Excellent", "true");
-            table.Rows.Add("7", "9", "5", "Excellent", "true");
-            table.Rows.Add("8", "9", "5", "Excellent", "true");
-            table.Rows.Add("9", "9", "5", "Excellent", "true");
-            table.Rows.Add("10", "9", "5", "Excellent", "true");
-            table.Rows.Add("11", "9", "5", "Excellent", "true");
-            table.Rows.Add("12", "9", "5", "Excellent", "true");
-            table.Rows.Add("13", "9", "5", "Excellent", "true");
-            table.Rows.Add("14", "9", "5", "Excellent", "true");
-            table.Rows.Add("15", "9", "5", "Excellent", "true");
-            table.Rows.Add("16", "9", "5", "Excellent", "true");
-            table.Rows.Add("17", "9", "5", "Excellent", "true");
-            table.Rows.Add("18", "9", "5", "Excellent", "true");
-            table.Rows.Add("19", "9", "5", "Excellent", "true");
-            table.Rows.Add("20", "9", "5", "Excellent", "true");
-            table.Rows.Add("21", "9", "5", "Excellent", "true");
-            table.Rows.Add("22", "9", "5", "Excellent", "true");
-            table.Rows.Add("23", "9", "5", "Excellent", "true");
-            table.Rows.Add("24", "9", "5", "Excellent", "true");
-            table.Rows.Add("25", "9", "5", "Excellent", "true");
+            table.Rows.Add("2", "6", "4", "Excellent", "true");
+            table.Rows.Add("3", "2", "3", "Excellent", "true");
+            table.Rows.Add("4", "7", "3", "Excellent", "false");
+            table.Rows.Add("5", "6", "5", "Excellent", "true");
+            table.Rows.Add("6", "3", "1", "Excellent", "false");
+            table.Rows.Add("7", "8", "1", "Excellent", "false");
+            table.Rows.Add("8", "9", "4", "Excellent", "true");
+            table.Rows.Add("9", "9", "5", "Excellent", "false");
+            table.Rows.Add("10", "5", "5", "Excellent", "true");
+            table.Rows.Add("11", "4", "1", "Excellent", "false");
+            table.Rows.Add("12", "9", "2", "Excellent", "true");
+            table.Rows.Add("13", "4", "2", "Excellent", "false");
+            table.Rows.Add("14", "4", "1", "Excellent", "true");
+            table.Rows.Add("15", "8", "5", "Excellent", "true");
+            table.Rows.Add("16", "7", "4", "Excellent", "false");
+            table.Rows.Add("17", "1", "4", "Excellent", "true");
+            table.Rows.Add("18", "1", "3", "Excellent", "true");
+            table.Rows.Add("19", "6", "5", "Excellent", "false");
+            table.Rows.Add("20", "8", "3", "Excellent", "true");
+            table.Rows.Add("21", "2", "4", "Excellent", "true");
+            table.Rows.Add("22", "3", "2", "Excellent", "false");
+            table.Rows.Add("23", "3", "1", "Excellent", "false");
+            table.Rows.Add("24", "7", "1", "Excellent", "false");
+            table.Rows.Add("25", "7", "4", "Excellent", "true");
 
             Data_table D = new Data_table();
-            D.DisplayProducts(table);
-
+            // D.DisplayProducts(table);
+            D.Display_IsLike_True(table);
         }
 
         public void DisplayProducts(DataTable dt)
         {
             var prodName = from products in dt.AsEnumerable()
+                           select products.Field<String>("ProductID");
+            foreach (var prd in prodName)
+            {
+                Console.WriteLine(prd);
+            }
+        }
+
+        public void Display_IsLike_True(DataTable dt)
+        {
+            var prodName = from products in dt.AsEnumerable() where products.Field<String>("IsLike") == "true"
                            select products.Field<String>("ProductID");
             foreach (var prd in prodName)
             {
