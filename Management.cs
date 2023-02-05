bf9b1c752253208cb552bbe7233b8f9bce40378f
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-02-05 20:52:40 +0530

    added a method to print selective record

--- Management.cs
+++ Management.cs
@@ -18,5 +18,14 @@ namespace Product_Review_Management
             }
         
         }
+
+        public void SelectRecords(List<Product_review> lpr)
+        {
+            var recordData= from p in lpr where(p.ProductID==1|| p.ProductID==4|| p.ProductID==9)&&p.Rating>3 select p;
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product ID : " + list.ProductID + "UserID : " + list.UserID + "Rating : " + list.Rating + "Review : " + list.Review + "IsLike : " + list.IsLike);
+            }
+        }
     }
 }
