d904c1e7b7d0543d01632a34c871b4ee3078c85c
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-02-05 20:20:15 +0530

    adding a model class

---
+++ Product_review.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Review_Management
+{
+    public class Product_review
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
